b0c59c152442ffc3dcc72e81a4ad00eb254b3b57
Mr.Snape <dvtientai@gmail.com>
2018-11-30 09:12:58 +0700

    update js add color-picker

--- btvn2-js-basic/main.js
+++ btvn2-js-basic/main.js
@@ -1,3 +1,5 @@
+
+/** control block **/
 // get initial value
 var parentObj = document.getElementById("parent-block");
 var parentWidth = parentObj.offsetWidth;
@@ -24,4 +26,33 @@ slider.oninput = function(){
 	var realDistance = realMinVal + Math.round(currentVal/100*maxDistance);
 	console.log("realDistance", realDistance);
 	childObj.style.marginLeft = realDistance+"px";
+}
+
+/** color picker **/
+var red = document.getElementById("red").value;
+var green = document.getElementById("green").value;
+var blue = document.getElementById("blue").value;
+var objDisplay = document.getElementById("color-display");
+objDisplay.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+var color_code = "#" + get_code(red) + get_code(green) + get_code(blue);
+document.getElementById("color-code").innerHTML = color_code ;
+
+rgbObj = document.getElementsByClassName("color");
+for (var i=0; i<rgbObj.length; ++i){
+	rgbObj[i].oninput = function(){
+		red = document.getElementById("red").value;
+		green = document.getElementById("green").value;
+		blue = document.getElementById("blue").value;
+		console.log(red,green, blue);
+		objDisplay.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+
+		var color_code = "#" + get_code(red) + get_code(green) + get_code(blue);
+		document.getElementById("color-code").innerHTML = color_code ;
+	}
+}
+
+function get_code(num){
+	var hexNum = parseInt(num).toString(16);
+	//add leading zero
+	return ("00"+hexNum).slice(-2);
 }
